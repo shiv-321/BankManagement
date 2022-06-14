f82dd1ae34552a19d0b1fb39ea93b5853b6588ef
Shiv <shivaraj.thapa2021@gmail.com>
2022-06-14 08:24:39 -0500

    created account

--- BankingSystem.java
+++ BankingSystem.java
@@ -2,15 +2,19 @@ package banking_project;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class BankingSystem {
-
+	
+	public static Connection c;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		
 		try {
+			c= Connect.connet();
 			Scanner sc = new Scanner(System.in);
 			System.out.println("1. New user");
 			System.out.println("2. Existing user");
@@ -28,10 +32,31 @@ public class BankingSystem {
 					String accNum = util.accountNumber();
 					System.out.println("Your account is created");
 					System.out.println("Your account Number is: " + accNum);
+					PreparedStatement ps = c.prepareStatement("insert into account values (?,?,?,?)");
+					ps.setString(1, accNum);
+					ps.setString(2, name);
+					ps.setString(3, password);
+					ps.setInt(4, 0);
+					ps.execute();
 					
 					
 				}else {
 					System.out.println("Password not matching");
+				}
+			}else {
+				System.out.println("Enter your acc number");
+				String accNums = sc.nextLine();
+				System.out.println("Enter your password");
+				String password = sc.nextLine();
+				ResultSet rs = c.prepareStatement("select password from account where accnum = " + accNums).executeQuery();
+				if(rs.next()) {
+					if(password.equals(rs.getString(1)))
+						System.out.println("login successful");
+					else 
+						System.out.println("Password mismatched");
+				}
+				else {
+					System.out.println("Account number doesnot exit");
 				}
 			}
 		
--- Connect.java
+++ Connect.java
@@ -7,7 +7,7 @@ public class Connect {
 	
 	public static Connection connet() {
 		try {
-			Class.forName("Oracle.jdbc.driver.OracleDriver");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection c = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE", "shivaraj1", "jstmaself");
 			System.out.println("Connected");
 			return c;
